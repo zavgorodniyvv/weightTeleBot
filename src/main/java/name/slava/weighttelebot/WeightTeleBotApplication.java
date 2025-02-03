2a7396ae8caeb9b4777dd646874ca1540716470f
slava <zavgorodniy@gmail.com>
2025-02-03 14:06:44 +0200

    Initial commit

---
+++ src/main/java/name/slava/weighttelebot/WeightTeleBotApplication.java
@@ -0,0 +1,13 @@
+package name.slava.weighttelebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightTeleBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeightTeleBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/name/slava/weighttelebot/WeightTeleBotApplicationTests.java
@@ -0,0 +1,13 @@
+package name.slava.weighttelebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightTeleBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
